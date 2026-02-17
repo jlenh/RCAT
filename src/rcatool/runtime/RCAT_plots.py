859a963c97c18fd7f58ab864701651d14429ae19
jlenh <julien.lenhardt@gmail.com>
2026-02-17 16:57:41 +0100

    Add support for plotting PDF for single model input

--- src/rcatool/runtime/RCAT_plots.py
+++ src/rcatool/runtime/RCAT_plots.py
@@ -1720,6 +1720,11 @@ class PlotConfiguration(object):
                            [f'{m.upper()} - {self.ref_model.upper()}'
                             for m in self.othr_mod]]
 
+            # In case of only one model as input and no comparison
+            #   = unlist the array corresponding to the dataset.
+            if not (self.othr_mod or (self.ref_obs is not None)):
+                dlist[0] = dlist[0][0]
+
             thr = fmod[self.ref_model].attrs['Description'].\
                 split('|')[1].split(':')[1].strip()
             regnm = reg.replace(' ', '_')
@@ -1731,8 +1736,10 @@ class PlotConfiguration(object):
             fn = self.define_file_names(thr, 'lnplot', region=regnm)
 
             # figure settings
-            figsize = (19, 8)
-            figshape = (1, 2)
+            figsize = (19, 8) if self.othr_mod or (self.ref_obs is not None)\
+                else (9, 8)
+            figshape = (1, 2) if self.othr_mod or (self.ref_obs is not None)\
+                else (1, 1)
 
             ylabel = ['Frequency (%)', 'Difference']
             ylim = [None]*2
@@ -1750,18 +1757,20 @@ class PlotConfiguration(object):
 
             [ln.set_color(lc) for ln, lc in zip(
                 list(axs[0].get_lines())[:len(dlist[0])], self.abs_colors)]
-            [ln.set_color(lc) for ln, lc in zip(
-                list(axs[1].get_lines())[:len(dlist[1])], self.rel_colors)]
+            if self.othr_mod or (self.ref_obs is not None):
+                [ln.set_color(lc) for ln, lc in zip(
+                    list(axs[1].get_lines())[:len(dlist[1])], self.rel_colors)]
 
             # Legend
             legend_elements = [Line2D([0], [0], lw=2, color=c, label=l)
                                for c, l in zip(self.abs_colors, lg_lbls[0])]
             axs[0].legend(handles=legend_elements,
                           fontsize='x-large', framealpha=.5)
-            legend_elements = [Line2D([0], [0], lw=2, color=c, label=l)
-                               for c, l in zip(self.rel_colors, lg_lbls[1])]
-            axs[1].legend(handles=legend_elements,
-                          fontsize='x-large', framealpha=.5)
+            if self.othr_mod or (self.ref_obs is not None):
+                legend_elements = [Line2D([0], [0], lw=2, color=c, label=l)
+                                for c, l in zip(self.rel_colors, lg_lbls[1])]
+                axs[1].legend(handles=legend_elements,
+                            fontsize='x-large', framealpha=.5)
 
             [rpl.axes_settings(ax, xlabel=xlabel[a], xticks=xticks,
                                ylabel=ylabel[a], xtlabels=xtlbls,
