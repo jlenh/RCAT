3b8cae89353e6c624962df46bdeb89e79794a648
jlenh <julien.lenhardt@gmail.com>
2026-09-02 21:35:34 +0200

    Add full domain support for moments timeseries

--- src/rcatool/runtime/RCAT_main.py
+++ src/rcatool/runtime/RCAT_main.py
@@ -1298,6 +1298,7 @@ def get_plot_dict(cdict, var, grid_coords, models, obs, tsuffix_dict, tres,
         'line grid setup': cdict['line grid setup'],
         'line kwargs': cdict['line kwargs'],
         'regions': cdict['regions'],
+        'full domain': cdict['full domain'],
         'time suffix dict': tsuffix_dict,
         'img dir': os.path.join(img_outdir, stat_name.replace(' ', '_'))
     }
